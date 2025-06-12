409ad63b933584a77355fb650256209609e6fdf9
Ondřej Tůma <ondrej.tuma.024@pslib.cz>
2025-06-12 11:58:24 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,72 @@
+radio.setGroup(124)
+radio.setFrequencyBand(52)
+
 type IRC = {
     l: DigitalPin,
     c: DigitalPin,
     r: DigitalPin
-}
-type PinData = {
-    l: boolean,
-    c: boolean,
-    r: boolean
 }
 const IR: IRC = {
     l: DigitalPin.P14,
     c: DigitalPin.P15,
     r: DigitalPin.P13
 }
+
+function drive(leftSpeed: number, rightSpeed: number) {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, leftSpeed)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, rightSpeed)
+}
+
+function isIntersection(): boolean {
+    let count = dataLeft + dataCenter + dataRight
+    if (count >= 2) {
+        return true
+    } else {
+        return false
+    }
+}
+
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
 
-let data: PinData = {l: false, c: false, r: false};
-basic.forever(function () {
-    data.l = pins.digitalReadPin(IR.l) === 1;
-    data.c = pins.digitalReadPin(IR.c) === 1;
-    data.r = pins.digitalReadPin(IR.r) === 1;
-    console.log(data);
+let dataLeft: number
+let dataRight: number
+let dataCenter: number
+let turn: string
 
+basic.forever(function () {
+    dataLeft = pins.digitalReadPin(IR.l)
+    dataRight = pins.digitalReadPin(IR.r);
+    dataCenter = pins.digitalReadPin(IR.c);
+    
+    if (isIntersection()) {
+        if (turn === "L") {
+            drive(0, -255)
+            basic.pause(500)
+        } else if (turn === "C") {
+            drive(-255, -150)
+            basic.pause(500)
+        } else if (turn === "R") {
+            drive(-255, 0)
+            basic.pause(500)
+        }
+    } else {
+        if (dataCenter === 1) {
+            drive(-255, -150)
+        } else if (dataLeft === 1) {
+            drive(0, -255)
+        } else if (dataRight === 1) {
+            drive(-255, 0)
+        } else {
+            drive(100, 100)
+        }
+    }
+
+    console.log(turn)
     basic.pause(20)
+})
+
+radio.onReceivedString(function (receivedString: string) {
+    turn = receivedString
 })
